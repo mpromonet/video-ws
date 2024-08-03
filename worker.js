aeaf5a9c6a14ddd6968a978fc9c5686ba0068daa
Michel Promonet <michel.promonet@free.fr>
2024-08-03 20:26:13 +0200

    move video-ws

---
+++ worker.js
@@ -0,0 +1,16 @@
+/* ---------------------------------------------------------------------------
+** This software is in the public domain, furnished "as is", without technical
+** support, and with no warranty, express or implied, as to its usefulness for
+** any purpose.
+**
+** -------------------------------------------------------------------------*/
+
+import { MediaStream } from './mediastream.js';
+
+self.onmessage = (event) => {
+    const offscreenCanvas = event.data.canvas;
+    console.log('worker received canvas', offscreenCanvas);
+
+    const mediaStream = new MediaStream(offscreenCanvas, null, (loaded) => postMessage({type: 'loaded', loaded}));
+    mediaStream.connect(event.data.url);
+};
